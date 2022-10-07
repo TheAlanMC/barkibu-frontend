27700a714782d69a4ec945929fc7fc8a1cef6ada
Chris Apaza <chrisalanapazaa@gmail.com>
2022-10-07 18:26:09 -0400

    Added veterinary profile information

--- lib/screens/veterinary_profile/veterinary_profile_screen.dart
+++ lib/screens/veterinary_profile/veterinary_profile_screen.dart
@@ -17,13 +17,22 @@ class VeterinaryProfileScreen extends StatelessWidget {
           )
         ],
       ),
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.all(10),
-          child: Column(children: [
-            Card(child: _profileInfo()),
-          ]),
-        ),
+      body: CustomScrollView(
+        slivers: [
+          SliverFillRemaining(
+            hasScrollBody: false,
+            child: Column(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
+              Card(child: _profileInfo()),
+              Card(child: _veterinarianRanking()),
+              Card(child: _aboutMe()),
+              Card(child: _reputation(context)),
+              Card(child: _profileInfo()),
+              Card(child: _veterinarianRanking()),
+              Card(child: _aboutMe()),
+              Card(child: _reputation(context)),
+            ]),
+          )
+        ],
       ),
       bottomNavigationBar: const CustomBottomNavigationVeterinary(
         currentIndex: 0,
@@ -37,7 +46,7 @@ class VeterinaryProfileScreen extends StatelessWidget {
       child: Row(
         mainAxisAlignment: MainAxisAlignment.start,
         children: [
-          CustomCircleAvatar(
+          const CustomCircleAvatar(
             border: AppTheme.secondary,
             path: 'assets/veterinary_profile.jpg',
           ),
@@ -54,6 +63,102 @@ class VeterinaryProfileScreen extends StatelessWidget {
               ),
             ],
           )
+        ],
+      ),
+    );
+  }
+
+  Widget _veterinarianRanking() {
+    return Padding(
+      padding: const EdgeInsets.all(15),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: const [
+              Text('#10', style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+              Text('RANKING MENSUAL', style: TextStyle(fontSize: 16)),
+            ],
+          ),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: const [
+              Text('#20', style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+              Text('RANKING GENERAL', style: TextStyle(fontSize: 16)),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _aboutMe() {
+    return Padding(
+      padding: const EdgeInsets.all(15),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: const [
+          SizedBox(child: Text('Acerca de mi:', style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold))),
+          Text(
+            //TODO: MAX 200 CHARACTERS
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nunc vel tincidunt lacinia, nunc nisl aliquam nisl, eget aliquam nisl nunc vel nisl. Sed euismod, nunc vel tincidunt lacinia, nunc nisl aliquam nisl, eget aliquam nisl nunc vel nisl.',
+            style: TextStyle(fontSize: 16),
+            textAlign: TextAlign.justify,
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget _reputation(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(15),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const Text(
+            'Reputación:',
+            style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+          ),
+          Row(
+            children: const [
+              SizedBox(width: 50, child: Text('50', style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold))),
+              Expanded(child: Text('Respuestas', style: TextStyle(fontSize: 16))),
+            ],
+          ),
+          Row(
+            children: const [
+              SizedBox(width: 50, child: Text('100', style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold))),
+              Expanded(
+                  child: Text('Dueños de mascotas han apoyado mis respuestas',
+                      textAlign: TextAlign.justify, style: TextStyle(fontSize: 16))),
+            ],
+          ),
+          Row(
+            children: const [
+              SizedBox(width: 50, child: Text('50', style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold))),
+              Expanded(
+                  child: Text('Colegas veterinarios han apoyado mis respuestas',
+                      textAlign: TextAlign.justify, style: TextStyle(fontSize: 16))),
+            ],
+          ),
+          const Text(
+            'Mascotas ayudadas:',
+            style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+          ),
+          Row(
+            children: const [
+              SizedBox(width: 50, child: Text('50', style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold))),
+              Expanded(child: Text('Perros', style: TextStyle(fontSize: 16))),
+            ],
+          ),
+          Row(
+            children: const [
+              SizedBox(width: 50, child: Text('100', style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold))),
+              Expanded(child: Text('Gatos', style: TextStyle(fontSize: 16))),
+            ],
+          ),
         ],
       ),
     );
