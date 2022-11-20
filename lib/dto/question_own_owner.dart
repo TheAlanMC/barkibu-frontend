d46b4ec9c3a347e9e2bcb56c9cfa1e90f091bd25
AndrePxd <andrepanique@gmail.com>
2022-11-20 01:01:07 -0400

    added Question own owner dto

---
+++ lib/dto/question_own_owner.dart
@@ -0,0 +1,29 @@
+import 'dart:convert';
+
+class OwnerOwnQuestionDto {
+  OwnerOwnQuestionDto({
+    required this.questionId,
+    required this.photoPath,
+    required this.problem,
+    required this.detailedDescription,
+    required this.questionDate,
+  });
+
+  int questionId;
+  String photoPath;
+  String problem;
+  String detailedDescription;
+  DateTime questionDate;
+
+  OwnerOwnQuestionDto fromJson(String str) =>
+      OwnerOwnQuestionDto.fromMap(json.decode(str));
+
+  factory OwnerOwnQuestionDto.fromMap(Map<String, dynamic> json) =>
+      OwnerOwnQuestionDto(
+        questionId: json["questionId"],
+        photoPath: json["photoPath"],
+        problem: json["problem"],
+        detailedDescription: json["detailedDescription"],
+        questionDate: DateTime.parse(json["questionDate"]),
+      );
+}
