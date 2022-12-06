4173a4ad729d709b53bb3a7bf763a600acef477a
Chris Apaza <chrisalanapazaa@gmail.com>
2022-12-06 16:46:01 -0400

    refactor files

--- lib/services/services.dart
+++ lib/services/services.dart
@@ -16,5 +16,5 @@ export 'package:barkibu/services/pet_treatment_service.dart';
 
 //String baseUrl = 'http://192.168.0.10:7777';
 //String baseUrl = 'http://192.168.1.161:7777';
-//String baseUrl = 'http://192.168.0.29:7777';
-String baseUrl = 'http://192.168.43.178:7777';
+String baseUrl = 'http://192.168.0.29:7777';
+// String baseUrl = 'http://192.168.43.178:7777';
